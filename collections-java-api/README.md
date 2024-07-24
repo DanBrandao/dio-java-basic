4306975ef019bbc0176bb83031ad44ed553dc832
danielbrandao <dpereira770@gmail.com>
2024-07-24 16:25:07 -0300

    Resolvendo exercicio proposto

---
+++ collections-java-api/README.md
@@ -0,0 +1,3 @@
+# Atividade sobre List DIO
+
+Atividades que eu repliquei ou pratiquei a partir do Bootcamp pela DIO em 2024
---
+++ collections-java-api/src/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,11 @@
+package list.OperacoesBasicas;
+
+import java.util.ArrayList;
+
+public class CarrinhoDeCompras {
+    private List<Item> itemList;
+
+    public ListaItem() {
+        this.itemList = new ArrayList<>();
+    }
+}
